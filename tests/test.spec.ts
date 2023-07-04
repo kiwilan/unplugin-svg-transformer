9f9edf194dc55f38fc43ec233000739edaace81b
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-04 08:24:53 +0200

    init

---
+++ tests/test.spec.ts
@@ -0,0 +1,8 @@
+// import svgPlugin from '../svg-plugin';
+import { describe, it } from 'vitest'
+
+describe('svgPlugin', () => {
+  it('should do something', () => {
+    // Write your test here
+  })
+})
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import svgPlugin from './src'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    vue(),
+    svgPlugin({
+      // Add your plugin options here
+    }),
+  ],
+})

bec6d4110490801f678f265b5b8d9bf9aa6d5fb9
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-04 17:25:22 +0200

    test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,17 +1,36 @@
 import fs from 'node:fs/promises'
+import os from 'node:os'
 import { describe, expect, it } from 'vitest'
 import { SvgItem } from '../src/SvgItem'
 import { FileUtils } from '../src/FileUtils'
 import { TsConverter } from '../src/TsConverter'
 
-const iconsDir = `${process.cwd()}/tests/icons/svg`
-const cacheDir = `${process.cwd()}/tests/icons/cache`
-const filenamePath = `${process.cwd()}/tests/icons/icons.ts`
-const gitignorePath = `${process.cwd()}/tests/icons/.gitignore`
+function getPaths() {
+  const iconsDir = `${process.cwd()}/tests/icons/svg`
+  const cacheDir = `${process.cwd()}/tests/icons/cache`
+  const filenamePath = `${process.cwd()}/tests/icons/icons.ts`
+  const gitignorePath = `${process.cwd()}/tests/icons/.gitignore`
+
+  if (os.platform() === 'win32') {
+    return {
+      iconsDir: iconsDir.replace(/\//g, '\\'),
+      cacheDir: cacheDir.replace(/\//g, '\\'),
+      filenamePath: filenamePath.replace(/\//g, '\\'),
+      gitignorePath: gitignorePath.replace(/\//g, '\\'),
+    }
+  }
+
+  return {
+    iconsDir,
+    cacheDir,
+    filenamePath,
+    gitignorePath,
+  }
+}
 
 describe('svgPlugin', () => {
   it('can parse and transform svg files', async () => {
-    const files = await SvgItem.toList(iconsDir, iconsDir)
+    const files = await SvgItem.toList(getPaths().iconsDir, getPaths().iconsDir)
     const twitter = files.find(file => file.getFilename() === 'twitter')
 
     expect(files.length).toBe(22)
@@ -24,8 +43,8 @@ describe('svgPlugin', () => {
     expect(twitter?.getContent()).toBe('<svg style="display: inline-block; height: inherit; width: inherit;" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>')
   })
   it('can write svg as ts files', async () => {
-    const files = await SvgItem.toList(iconsDir, iconsDir)
-    await FileUtils.writeSvgAsTs(files, cacheDir)
+    const files = await SvgItem.toList(getPaths().iconsDir, getPaths().iconsDir)
+    await FileUtils.writeSvgAsTs(files, getPaths().cacheDir)
 
     const path = './tests/icons/cache/social/twitter.ts'
     const content = await FileUtils.read(path)
@@ -33,8 +52,8 @@ describe('svgPlugin', () => {
     expect(content).toBe('export default \'<svg style="display: inline-block; height: inherit; width: inherit;" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>\'\n')
   })
   it('can create the icons file', async () => {
-    const files = await SvgItem.toList(iconsDir, iconsDir)
-    await TsConverter.make(files, iconsDir, cacheDir, filenamePath)
+    const files = await SvgItem.toList(getPaths().iconsDir, getPaths().iconsDir)
+    await TsConverter.make(files, getPaths().iconsDir, getPaths().cacheDir, getPaths().filenamePath)
 
     const path = './tests/icons/icons.ts'
     const content = await FileUtils.read(path)
@@ -68,13 +87,13 @@ export const IconList: Record<IconType | string, Promise<{ default: string }>> =
   })
   it('can create the gitignore file', async () => {
     // delete `.gitignore` file
-    await fs.rm(gitignorePath, { force: true })
+    await fs.rm(getPaths().gitignorePath, { force: true })
 
     // create `.gitignore` file
-    await fs.writeFile(gitignorePath, '')
+    await fs.writeFile(getPaths().gitignorePath, '')
 
-    await FileUtils.addPathToGitignoreIfNotExists(cacheDir, gitignorePath)
-    const content = await FileUtils.read(gitignorePath)
+    await FileUtils.addPathToGitignoreIfNotExists(getPaths().cacheDir, getPaths().gitignorePath)
+    const content = await FileUtils.read(getPaths().gitignorePath)
 
     expect(content).toBe('\n/tests/icons/cache')
   })

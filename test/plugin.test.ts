edd4d8638b541b34977efa6e4c8f252a742019e9
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-09-26 17:17:23 +0200

    tests

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -19,24 +19,24 @@ it('can parse and transform svg files', async () => {
   expect(twitter?.getContent()).toBe('<svg style="height: inherit; width: inherit;" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>')
 })
 
-it('can write svg as ts files', async () => {
-  const paths = getPaths()
-  const writer = await SvgTransformer.make({
-    iconsDir: paths.iconsDir,
-    libraryDir: paths.libraryDir,
-    types: true,
-    windowInject: true,
-  })
+// it('can write svg as ts files', async () => {
+//   const paths = getPaths()
+//   const writer = await SvgTransformer.make({
+//     iconsDir: paths.iconsDir,
+//     libraryDir: paths.libraryDir,
+//     useTypes: true,
+//     // windowInject: true,
+//   })
 
-  expect(typeof writer).toBe('object')
-  //   const files = await SvgItem.toList(getPaths().iconsDir)
-  //   await SvgItem.listToTsFiles(files, getPaths().cacheDir)
+//   expect(typeof writer).toBe('object')
+//   //   const files = await SvgItem.toList(getPaths().iconsDir)
+//   //   await SvgItem.listToTsFiles(files, getPaths().cacheDir)
 
-  //   const path = './test/icons/cache/social/twitter.ts'
-  //   const content = await Path.read(path)
+//   //   const path = './test/icons/cache/social/twitter.ts'
+//   //   const content = await Path.read(path)
 
-//   expect(content).toBe('export default \'<svg style="display: inline-block; height: inherit; width: inherit;" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>\'\n')
-})
+// //   expect(content).toBe('export default \'<svg style="display: inline-block; height: inherit; width: inherit;" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>\'\n')
+// })
 
 // it('can create the icons file', async () => {
 //   const files = await SvgItem.toList(getPaths().iconsDir)
